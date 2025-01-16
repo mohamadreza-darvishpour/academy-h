dfc6c89fecd9154a179bf27bbddf4d5beb2ddcaf
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2025-01-17 02:24:18 +0330

    start near final

---
+++ a_python_programming/train.py
@@ -0,0 +1,145 @@
+
+
+
+exceptions    
+
+for entry in list :
+    try: 
+        lab 
+        lab 
+    except exception as e :
+        print('exception: ' , e.__class__ , 'occured')
+
+
+raise KeyboardInterrupt
+assert num/2 = 0 , ....
+default exceptions 
+except(TypeError ,  ZeroDivisionError , ...)
+except TypeError 
+except ValueError 
+except ZeroDivisionError
+and  ....
+
+# bat file      *****************888
+
+
+
+
+
+
+# f = open('path/test.txt' , encoding='utf-8') 
+# f.close()
+# f.namef.mode 
+# f.write() 
+# f.read() 
+# f.readline 
+# f.readlines 
+
+
+
+
+
+
+
+
+# import pandas as pd 
+# import matplotlib.pyplot as plt 
+# import numpy as np 
+
+
+
+
+
+
+
+
+# list1 = [8,9,10 , 11 ] 
+# list(filter(lambda x : x>8329 , list1))
+
+
+# eg = map(lambda x ,y :x*x+y , anylist , anylist2 )  
+
+
+# lambda functioons   
+
+
+
+
+
+# ypoints = np.array([1,2,3,4,565])
+# plt.plot(ypoints) 
+# plt.show()  
+
+
+# x = np.array([1,2,3]) 
+# y = np.array([2,4,6])
+# plt.plot(x , y) 
+# plot.subplot
+# plt.scatter
+# plt.colorbar  
+# plt.bar    
+# plt.histplt.xticks
+# plt.legend
+
+# lf = lambda x : x**2 +82983 
+# group.transform(lf)
+
+# تابع ریمان 
+
+
+
+
+# data_dic = {'one':[1,3,5] , 'two':[6,8,9]}   
+# frame = pd.DataFrame(data_dic) 
+# set1 = [3,4,5,6,7,7,8,888] 
+# series = pd.Series(set1)
+
+# dic2 = {1:0 , 2:991 , 7:829 , 9 : 92839342} 
+# ser2 = pd.Series(dic2) 
+# print(ser2)   
+
+# ser3 = pd.Series(dic2 , index=[1 , 7, 9])
+
+# print(ser3)
+# print(ser3.loc[7])   #read with index name or number use 'loc' 
+
+
+# #change dataframe to csv 
+# # csv = frame.to_csv('new_name_file ' )
+# frame = pd.read_csv('path-of-file')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# set0 = set([3,4,5,6,7])
+# set1 = set([3,66,88,6,7])
+
+# set0.union 
+# set0.add(set1) 
+# set0.intersection(set1)
+# set0.difference(set1) 
+# set0.symmetric_difference(set1) 
+# #....
+
+# print(set)
+
+
+
+
+
+
