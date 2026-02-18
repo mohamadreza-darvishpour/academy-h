bf9cdb8960cc5d686c488d4f3a1119ae0b37ed77
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2026-02-18 15:07:09 +0330

    more in regression

--- e_supervised_ml/codes/5_regression.py
+++ e_supervised_ml/codes/5_regression.py
@@ -21,6 +21,13 @@ import seaborn as sns        # use to alter chart
 
 from sklearn.datasets import make_regression 
 from sklearn.model_selection import train_test_split
+sns.set_style('whitegrid')
+from sklearn.linear_model import LinearRegression
+
+'''
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html#sklearn.linear_model.LinearRegression
+use this linear model
+'''
 
 
 
@@ -31,14 +38,29 @@ x_train , x_test , y_train ,y_test = train_test_split(X , Y , test_size=0.2)
 #draw the chart 
 plt.scatter(X,Y)
 plt.title('made dataset')
-plt.show()
+
 
 plt.scatter(x_train,y_train)
 plt.title('train dataset')
-plt.show()
+
 
 plt.scatter(x_test , y_test)
 plt.title('test dataset')
-plt.show()
 
+
+
+L_R = LinearRegression()
+L_R.fit(x_train , y_train)
+
+# y_pred = L_R.predict(x_test)
+score = L_R.score(x_test , y_test)        
+print('\n' , score , '\n')
+
+_x_range = np.arange(-3 , +3 , 0.1)
+_x_range_re = np.reshape(_x_range , (60,1))
+#***
+print(_x_range.shape)
+y_pred = L_R.predict(_x_range_re)
+plt.plot(_x_range , y_pred)
+plt.show()
 
