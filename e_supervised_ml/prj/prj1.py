f0a9c173763e8878a7f91b3e9f0ba9688cba5907
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2026-02-19 19:30:17 +0330

    prc 1 done

--- e_supervised_ml/prj/prj1.py
+++ e_supervised_ml/prj/prj1.py
@@ -3,12 +3,18 @@ import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(strategy='mean')
+
+
+
+
 
 
 path = "prj/project/Dataset/CSM_dataset.xlsx"
 dataset = pd.read_excel(path)
 
-print(dataset.head(2))
+# print(dataset.head(2))
 
 #حذف سطر های خالی
 dset = dataset.dropna(how='all')
@@ -22,16 +28,24 @@ X = dset
  
 
 minmaxscaler = MinMaxScaler()
+X = imputer.fit_transform(X)
 X = minmaxscaler.fit_transform(X) 
 x_train , x_test , y_train , y_test = train_test_split(X , Y , test_size=0.2)
 
-print(x_train.shape , x_test.shape)     
+# print(x_train.shape , x_test.shape)     
+
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_squared_error
 
 
+sgd = SGDRegressor(max_iter=1000 , learning_rate='invscaling' , eta0=0.01 )
+sgd.fit(x_train , y_train)
 
 
+y_pred = sgd.predict(x_test)
 
-print(dset.head(2))    # 231*14
+print('mse :  ' , mean_squared_error(y_test , y_pred))
+
 
 
 
