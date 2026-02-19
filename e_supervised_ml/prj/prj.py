6f9681c63f0e22bbc6ce28dbc1d55b51f866935e
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2026-02-19 19:16:52 +0330

    prc 1

--- e_supervised_ml/prj/prj.py
+++ e_supervised_ml/prj/prj.py
@@ -1,23 +1,37 @@
 
 import numpy as np 
 import pandas as pd 
-
-
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.model_selection import train_test_split
 
 
 path = "prj/project/Dataset/CSM_dataset.xlsx"
 dataset = pd.read_excel(path)
 
-#حذف سطر های زوج خالی
+print(dataset.head(2))
+
+#حذف سطر های خالی
 dset = dataset.dropna(how='all')
 dset.columns = range(dset.shape[1]) # تغییر نام ستون‌ها به اعداد   
 
+#delete name col
+dset = dset.drop(columns=[0])
+
+Y = dset.pop(4)
+X = dset       
+ 
+
+minmaxscaler = MinMaxScaler()
+X = minmaxscaler.fit_transform(X) 
+x_train , x_test , y_train , y_test = train_test_split(X , Y , test_size=0.2)
+
+print(x_train.shape , x_test.shape)     
 
 
 
 
 
-print(dset)    # 231*14
+print(dset.head(2))    # 231*14
 
 
 
